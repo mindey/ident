7714fe5485b1293599f201d94954262107cfe49f
Mindey <mindey@qq.com>
2020-06-13 00:34:14 +0000

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-with open('README.md', 'r', encoding='utf-8') as f:
+with open('README.rst', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -8,7 +8,7 @@ setup(
     version='1.0',
     description='Identify with challenge messsage and SSH key.',
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    # long_description_content_type='text/markdown',
     url='https://github.com/mindey/ident',
     author='Mindey',
     author_email='~@mindey.com',
