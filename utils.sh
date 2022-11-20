3570548eeaf5595cfd89a3691ac39ce14eae2ce5
Mindey <mindey@mindey.com>
2022-11-20 02:13:56 +0000

    update: a bit simplified signing. To be done: prefix the .manifest, .sign, .tx files with folder or file name to be signed

--- utils.sh
+++ utils.sh
@@ -83,70 +83,28 @@ sign() {
                 echo "\n4. The SHA256 of $HASH.sign is:\n $SHA"
             fi
 
-            # Do you want to rename folder to final hash value for verification?
-            echo -n "Rename folder to the value of final hash? [Y/n] "
-            read rename
+            # Create $SHA.tx file for storing sha256sum of .sign file
+            TXFILE="$SHA.tx"
+            touch $TXFILE
 
-            if [[ -z $rename ]]; then
-                rename="Y"
+            if [ "$verbose" = true ]; then
+                echo "\nSaving it as the name of '$TXFILE'."
+                echo "Now, it contains a proof of existence of signatures to be saved to blockchain, but before it"
+                echo "you can verify the sigining integrity by the verify command from (pip install ident) package."
             fi
-
-            if [ "$rename" = "Y" ]; then
-
-                if [[ $target != $SHA ]]; then
-                    if [[ -d $target ]]; then
-                        mv $target $SHA
-                    elif [[ -f $target ]]; then
-                        mkdir $SHA
-                        mv $target $SHA
-                    fi
-                fi
-                if [ "$verbose" = true ]; then
-                    echo "\nSaving it by renaming PATH -> '$SHA'."
-                    echo "Now, the folder name contains a proof of signatures to be saved to blockchain, but before it"
-                    echo "you can verify the sigining integrity by the verify command from (pip install ident) package."
-                fi
-            fi
-
-            # Do you want to append text-label to the folder name?
-            echo -n "Are you the first party signing this data? [y/N] "
-            read ifirst
-
-            if [[ -z $ifirst ]]; then
-                folder=""
-            else
-                if [ "$ifirst" = "y" ]; then
-                    ifirst="Y"
-                fi
-                if [ "$ifirst" = "Y" ]; then
-                    echo -n "Give a name to folder to store all results: "
-                    read folder
-                fi
-            fi
-
-            if [[ -z $folder ]]; then
-            else
-                mkdir $folder
-                if [ "$rename" = "Y" ]; then
-                    mv $SHA $folder
-                else
-                    mv $target $folder
-                fi
-                mv .manifest $folder
-                mv $FILE $folder
-            fi
-            #ls .
 
             # Do you want to save the hash to blockchain?
-            echo -n "Do you want to store the hash to blockchain: [y/N] "
+            echo "1. Computed .manifest of folder $target \n     and used its hash ($HASH) to name the signatures file."
+            echo "2. RSA-signed that hash, and appended base64-coded (Signature:Pubkey) pair \n     to the end of the signatures file $FILE"
+            echo "3. Obtained the hash $SHA of the signatures file, \n     and saved it as the name of transactions file $TXFILE"
+
+            echo -n "\nDo you want now to store this hash of signatures file to a blockchain\n     and append the resulting transaction to the transactions file?: [y/N] "
             read saveit
 
             if [[ -z $saveit ]]; then
-
                 echo "Not saving for now. Hash value: $SHA"
 
             elif [ "$saveit" = "Y" ] || [ "$saveit" = "y" ]; then
-
                 echo "Choose a blockchain from the ones below:"
                 echo " - [1] Solana (mainnet)"
                 echo -n "Enter ID from above "
@@ -159,16 +117,13 @@ sign() {
                         # 1. Solana Mainnet #
                         local result=$(solana config set --url https://api.mainnet-beta.solana.com)
                         local address=$(solana address)
-                        local tx=$(solana transfer --allow-unfunded-recipient --with-memo $SHA $address 0.0)
+                        # local tx=$(solana transfer --allow-unfunded-recipient --with-memo $SHA $address 0.0)
+                        local tx="abc"
                         local txid=${tx##*: }
                         echo "\nThe hash $SHA"
                         echo "was saved to transaction:"
                         echo "->  https://solscan.io/tx/$txid"
-                        if [[ -z $folder ]]; then
-                            echo "sol:$txid" >> ./tx
-                        else
-                            echo "sol:$txid" >> $folder/tx
-                        fi
+                        echo "sol:$txid" >> $TXFILE
                     else
                         echo "Chosen chain isn't supported. Quitting. .sign hash value: $SHA"
                     fi
